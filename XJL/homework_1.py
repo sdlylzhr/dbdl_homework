ea12e04d44c1552f540d9dcf091b90f9a6b91cba
Your Name <you@example.com>
2018-07-09 13:45:13 +0800

    随机分配teachers到rooms中

--- XJL/homework_1.py
+++ XJL/homework_1.py
@@ -1,1 +1,9 @@
-print("11111")
+import random
+
+rooms = [[], [], []]
+teachers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+
+for i in teachers:
+    a = random.randint(0, 2)
+    rooms[a].append(i)
+print(rooms)
