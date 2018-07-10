951c9527a9361581079670e1b65f3ee64930875d
sdlylzhr@163.com <sdlylzhr@163.com>
2018-07-10 13:55:32 +0800

    代码标准修复

--- homew.py
+++ homew.py
@@ -6,7 +6,7 @@ teachers = ["A", "B", "C", "D", "E", "F", "G", "H"]
 # 3. 随机把 8 名老师的名字添加到 第 1 个列表中
 for name in teachers:
     # 生成一个 0 到 2 之间的随机数，用来进行随机分配办公室
-    randomNum = random.randint(0,2)
+    randomNum = random.randint(0, 2)
     # 向 randomNum 标记的 room 中添加一个新的老师名字
     rooms[randomNum].append(name)
 
