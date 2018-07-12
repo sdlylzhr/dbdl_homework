81172414ada32e028b699c38798075af56a847bc
sf <sfdbfs@gmail.com>
2018-07-12 15:36:35 +0800

    crawlerBook,函数返回URl,简化对函数返回值的处理

--- SF/crawlerBook.py
+++ SF/crawlerBook.py
@@ -7,12 +7,13 @@ def bookMarkList(url):
     result = request.urlopen(req).read()
     soup = BeautifulSoup(result, "html5lib")
 
-    bookmarklist = soup.find('div',{'class': 'bookmark-list'}).find("ul").find_all("li")
-    for li in bookmarklist:
-        h2 = li.find("h2")
-        url = "http://www.shicimingju.com"+h2.find("a").attrs['href']
-        print(h2.get_text()+"*********"+url)
-    return bookmarklist
+    book_mark_list = soup.find('div',{'class': 'bookmark-list'}).find("ul").find_all("li")
+    url_list = ["http://www.shicimingju.com"+liPath.find("h2").find("a").attrs['href'] for liPath in book_mark_list]
+    # for li in book_mark_list:
+    #     h2 = li.find("h2")
+    #     url = "http://www.shicimingju.com"+h2.find("a").attrs['href']
+    #     print(h2.get_text()+"*********"+url)
+    return url_list
 # bookMarkList("http://www.shicimingju.com/book/")
 
 # 书籍目录
@@ -24,11 +25,12 @@ def bookMulu(url):
     bookHeader = soup.find('div',{'class': 'book-header'})
     print(bookHeader.find("h1").get_text())
 
-    bookmulu = soup.find('div',{'class': 'book-mulu'}).find("ul").find_all("li")
-    for li in bookmulu:
-        url = "http://www.shicimingju.com"+li.find("a").attrs['href']
-        print(li.get_text()+"*********"+url)
-    return bookmulu
+    book_mu_lu = soup.find('div',{'class': 'book-mulu'}).find("ul").find_all("li")
+    url_list = ["http://www.shicimingju.com"+liPath.find("a").attrs['href'] for liPath in book_mu_lu]
+    # for li in book_mu_lu:
+    #     url = "http://www.shicimingju.com"+li.find("a").attrs['href']
+    #     print(li.get_text()+"*********"+url)
+    return url_list
 # bookMulu("http://www.shicimingju.com/book/sanguoyanyi.html")
 
 # 章节内容
@@ -41,21 +43,17 @@ def chapterContent(url):
     print(chaptercontent.find("h1").get_text())
 
     chapter_content = chaptercontent.find('div',{'class': 'chapter_content'}).find_all("p")
-    # temp = []
-    for p in chapter_content:
-        # temp.append(p.get_text())
-        print(p.get_text())
-    # print(temp)
-    return chapter_content
+    string_content = [pPath.get_text() for pPath in chapter_content]
+    # for p in chapter_content:
+    #     # temp.append(p.get_text())
+    #     print(p.get_text())
+    return string_content
 # chapterContent("http://www.shicimingju.com/book/sanguoyanyi/1.html")
 
 # 获得书籍选择页面
-for li in bookMarkList("http://www.shicimingju.com/book/"):
-    h2 = li.find("h2")
-    url = "http://www.shicimingju.com"+h2.find("a").attrs['href']
+for urlMark in bookMarkList("http://www.shicimingju.com/book/"):
     # 获得书籍目录页面
-    for li2 in bookMulu(url):
-        url = "http://www.shicimingju.com"+li2.find("a").attrs['href']
+    for urlMulu in bookMulu(urlMark):
         # 获得章节内容页面
-        for p in chapterContent(url):
-            print(p.get_text())
+        for content in chapterContent(urlMulu):
+            print(content)
