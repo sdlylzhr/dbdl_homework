1e5582aaf4da0c70c142f52b964ba2a25a594bf0
sf <sfdbfs@gmail.com>
2018-07-13 10:21:52 +0800

    读取20页,每页获取1920*1080的壁纸列表,下载相应壁纸,优化网页请求

--- SF/crawlerPicture.py
+++ SF/crawlerPicture.py
@@ -1,31 +1,60 @@
 from bs4 import BeautifulSoup
 from urllib import request
+import random
+import time
 
 # 全局变量
 global numPath
 global header
 numPath = 0
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.235'}
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.235'
+}
+
+
+# 打开网页
+def htmlUrl(url):
+    # 伪装请求报头
+    global header
+    # req = request.Request(url, headers=header)
+    # result = request.urlopen(req).read()
+    # 添加timeout异常处理
+    timeout = random.choice(range(80, 180))
+    req = request.Request(url, headers=header)
+    while True:
+        try:
+            result = request.urlopen(req, timeout = timeout).read()
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(3)
+    return result
+
 
 # 选择页面
 def pageChoice(num):
-    url = "https://www.walldevil.com/best-wallpapers"
+    url = "https://www.walldevil.com/best-wallpapers/"
     if num != 0:
-        url = url + "/page/%d"%num
+        url = url + "/page/%d" % num
     # print(url)
     return url
 # pageChoice(10)
 
+
 # 每页图片清单
 def pictures(url):
-    # 伪装请求报头
-    global header
-    req = request.Request(url, headers = header)
-    result = request.urlopen(req).read()
+    # header = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.235'
+    # }
+    # req = request.Request(url, headers=header)
+    # result = request.urlopen(req).read()
+    # 启用统一网页请求控制
+    result = htmlUrl(url)
     soup = BeautifulSoup(result, "html5lib")
 
-    imgsArticle = soup.find_all('article', {'class': 'col-xxl-2 col-xl-five col-lg-3 col-md-4 col-sm-6 col-xs-6 col-xxs-12'})
-    imgsDiv = [article.find('div',{'class':'inner'}) for article in imgsArticle]
+    imgsArticle = soup.find_all('article',
+                                {'class': 'col-xxl-2 col-xl-five col-lg-3 col-md-4 col-sm-6 col-xs-6 col-xxs-12'})
+    imgsDiv = [article.find('div', {'class': 'inner'}) for article in imgsArticle]
     imgsUrl = []
     for div in imgsDiv:
         # 筛选出图片属性
@@ -36,11 +65,10 @@ def pictures(url):
     return imgsUrl
 # pictures("https://www.walldevil.com/best-wallpapers/")
 
+
 # 获取单个图片下载地址
 def picture(url):
-    global header
-    req = request.Request(url, headers=header)
-    result = request.urlopen(req).read()
+    result = htmlUrl(url)
     soup = BeautifulSoup(result, "html5lib")
 
     imgFigure = soup.find("figure")
@@ -49,25 +77,25 @@ def picture(url):
     return imgUrl
 # picture("https://www.walldevil.com/galaxy-tumblr-quotes-wallpaper-874462")
 
+
 # 储存图片
 def writePicture(url):
     global numPath
-    global header
     try:
-        req = request.Request(url, headers=header)
-        result = request.urlopen(req).read()
-        f = open("C:\\Users\\SF\\Desktop\\picture\\%d.jpg"%numPath,"wb")
+        result = htmlUrl(url)
+        f = open("C:\\Users\\SF\\Desktop\\picture\\%d.jpg" % numPath, "wb")
         f.write(result)
         f.close()
         numPath += 1
     except Exception as e:
         print(e)
 
+
 # 壁纸下载(0~20)
 i = 0
-while i<20:
-    for picturesUrl in pictures(pageChoice(i)): # 获取当前页下载列表
-        downloadUrl = picture(picturesUrl)      # 获取单个下载项
+while i < 20:
+    for picturesUrl in pictures(pageChoice(i)):  # 获取当前页下载列表
+        downloadUrl = picture(picturesUrl)  # 获取单个下载项
         print(downloadUrl)
-        writePicture(downloadUrl)               # 存储下载文件
-    i +=1
+        writePicture(downloadUrl)  # 存储下载文件
+    i += 1
